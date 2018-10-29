7bf2602240f7c9b98250710c4731ee820bfa7e46
terfu.s <manabumql4@yahoo.co.jp>
2018-10-29 02:18:03 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  
   def index
     @tasks = Task.all
   end
+
   def show
-    @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
@@ -19,11 +23,11 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
   def edit
-    @task = Task.find(params[:id])
   end
+
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -33,14 +37,19 @@ class TasksController < ApplicationController
       render :edit
     end
   end
+
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:content)
   end
